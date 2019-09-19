16a0f8c34dd584a83f4c825e620c17ecc4baf2ef
raf <rafutek@protonmail.com>
2019-09-19 18:42:15 -0400

    Full screen with f key but no panels on sides

--- PacManProject/src/view/GameFrame.java
+++ PacManProject/src/view/GameFrame.java
@@ -13,8 +13,8 @@ public class GameFrame extends JFrame implements WindowListener
 {
 
 	private static final long serialVersionUID = 1L;
-
-
+	
+	private boolean fullScreen = false;
 	private RenderThread renderTh;
 
 	public GameFrame(int period)
@@ -24,7 +24,7 @@ public class GameFrame extends JFrame implements WindowListener
 		addWindowListener( this );
 
 		pack();
-		setResizable(false);
+		setResizable(true);
 		setLocationRelativeTo(null); // center the window
 		setVisible(true);
 		setFocusable(true);
@@ -33,74 +33,86 @@ public class GameFrame extends JFrame implements WindowListener
 	}  
 	
 
-/**
- * Create game and status bar panels, and the rendering thread
- * @param period
- */
-private void makeGUI(int period)
-{
-	Container c = getContentPane();    // default BorderLayout used
-
-	GamePanel gamePanel = new GamePanel();
-	c.add(gamePanel, "Center");
+	/**
+	 * Create game and status bar panels, and the rendering thread
+	 * @param period
+	 */
+	private void makeGUI(int period)
+	{
+		Container c = getContentPane();    // default BorderLayout used
 	
-	StatusBarPanel statusBarPanel = new StatusBarPanel();
-	c.add(statusBarPanel, "South");
-
-	renderTh = new RenderThread(period, gamePanel, statusBarPanel);
+		GamePanel gamePanel = new GamePanel();
+		c.add(gamePanel, "Center");
+		
+		StatusBarPanel statusBarPanel = new StatusBarPanel();
+		c.add(statusBarPanel, "South");
 	
-}  // end of makeGUI()
-
-/**
- * Called when the window listener i added to the window (before pack() in the constructor).
- * This method launch the render thread
- */
-public void addNotify()
-{ 
-	super.addNotify();   // creates the peer
-	renderTh.startGame();    // start the thread
-}
-
-private void readyForTermination()
-{
-	addKeyListener( new KeyAdapter() {
-		// listen for esc, q, end, ctrl-c on the canvas to
-		// allow a convenient exit from the full screen configuration
-		public void keyPressed(KeyEvent e)
-		{ 
-			int keyCode = e.getKeyCode();
-			if ((keyCode == KeyEvent.VK_ESCAPE) || (keyCode == KeyEvent.VK_Q) ||
-					(keyCode == KeyEvent.VK_END) ||
-					((keyCode == KeyEvent.VK_C) && e.isControlDown()) ) {
-				renderTh.stopGame();
+		renderTh = new RenderThread(period, gamePanel, statusBarPanel);
+		
+	}  // end of makeGUI()
+	
+	/**
+	 * Called when the window listener is added to the window.
+	 * This method launch the render thread
+	 */
+	public void addNotify()
+	{ 
+		super.addNotify();   // creates the peer
+		renderTh.startGame();    // start the thread
+	}
+	
+	private void readyForTermination()
+	{
+		addKeyListener( new KeyAdapter() {
+			public void keyPressed(KeyEvent e)
+			{ 
+				int keyCode = e.getKeyCode();
+				
+				// listen for esc, q, end, ctrl-c on the canvas to
+				// allow a convenient exit from the full screen configuration
+				if ((keyCode == KeyEvent.VK_ESCAPE) || (keyCode == KeyEvent.VK_Q) ||
+						(keyCode == KeyEvent.VK_END) ||
+						((keyCode == KeyEvent.VK_C) && e.isControlDown()) ) {
+					renderTh.stopGame();
+				}
+				// listen for f to go on full screen or normal sized window
+				else if (keyCode == KeyEvent.VK_F) {
+					if(!fullScreen) {
+						fullScreen = true;				
+						setExtendedState(JFrame.MAXIMIZED_BOTH);
+					}
+					else {
+						fullScreen = false;
+						setExtendedState(JFrame.NORMAL);
+					}
+				}
 			}
-		}
-	});
-}  // end of readyForTermination()
+		});
+	}  // end of readyForTermination()
 
 
 
 // ----------------- window listener methods -------------
 
-public void windowActivated(WindowEvent e) 
-{ renderTh.resumeGame();  }
-
-public void windowDeactivated(WindowEvent e) 
-{  renderTh.pauseGame();  }
-
-
-public void windowDeiconified(WindowEvent e) 
-{  renderTh.resumeGame();  }
-
-public void windowIconified(WindowEvent e) 
-{  renderTh.pauseGame(); }
-
-
-public void windowClosing(WindowEvent e)
-{  renderTh.stopGame();  }
-
-public void windowClosed(WindowEvent e) {}
-public void windowOpened(WindowEvent e) {}
+	public void windowActivated(WindowEvent e) 
+	{ renderTh.resumeGame();  }
+	
+	public void windowDeactivated(WindowEvent e) 
+	{  renderTh.pauseGame();  }
+	
+	
+	public void windowDeiconified(WindowEvent e) 
+	{  renderTh.resumeGame();  }
+	
+	public void windowIconified(WindowEvent e) 
+	{  renderTh.pauseGame(); }
+	
+	
+	public void windowClosing(WindowEvent e)
+	{  renderTh.stopGame();  }
+	
+	public void windowClosed(WindowEvent e) {}
+	public void windowOpened(WindowEvent e) {}
 
 
 
