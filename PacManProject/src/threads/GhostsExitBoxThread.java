33614a56391d6a14b4928ac18b26bf3ffdf03ad1
raf <rafutek@protonmail.com>
2019-10-01 19:18:19 -0400

    delete ghost exit thread console messages

--- PacManProject/src/threads/GhostsExitBoxThread.java
+++ PacManProject/src/threads/GhostsExitBoxThread.java
@@ -39,25 +39,21 @@ public class GhostsExitBoxThread extends TimerThread {
 	protected void doThatWhileWaiting() {
 		if(!ghostWantsToGoOut) {
 			if(blinky.isInTheBox) {
-				System.out.println("blinky wants to go out");
 				ghostWantsToGoOut = true;
 				blinkyWantsToGoOut = ghostWantsToGoOut;
 				counterWaits = 0; // reset the timer to make the ghost wait until...
 			}
 			else if(pinky.isInTheBox) {
-				System.out.println("pinky wants to go out");
 				ghostWantsToGoOut = true;
 				pinkyWantsToGoOut = ghostWantsToGoOut;
 				counterWaits = 0;
 			}
 			else if(clyde.isInTheBox) {
-				System.out.println("clyde wants to go out");
 				ghostWantsToGoOut = true;
 				clydeWantsToGoOut = ghostWantsToGoOut;
 				counterWaits = 0; 
 			}
 			else if(inky.isInTheBox) {
-				System.out.println("inky wants to go out");
 				ghostWantsToGoOut = true;
 				inkyWantsToGoOut = ghostWantsToGoOut;
 				counterWaits = 0; 
@@ -69,21 +65,18 @@ public class GhostsExitBoxThread extends TimerThread {
 		}
 		else if(ghostCanGoOut){ 
 			if(blinkyCanGoOut) {
-				System.out.println("blinky is going out !"); // ...and he get out
 				if(manageGhostExit(blinky)) {
 					blinkyWantsToGoOut = false;
 					blinkyCanGoOut = false;
 				}
 			}
 			else if(pinkyCanGoOut) {
-				System.out.println("pinky is going out !");
 				if(manageGhostExit(pinky)) {
 					pinkyWantsToGoOut = false;
 					pinkyCanGoOut = false;
 				}
 			}
 			else if(clydeCanGoOut) {
-				System.out.println("clyde is going out !");
 				if(manageGhostExit(clyde)) {
 					clydeWantsToGoOut = false;
 					clydeCanGoOut = false;
@@ -106,19 +99,15 @@ public class GhostsExitBoxThread extends TimerThread {
 			ghostCanGoOut = true;
 			
 			if(blinkyWantsToGoOut) {
-				System.out.println("blinky can go out !"); // ...until he can get out and...
 				blinkyCanGoOut = true;
 			}
 			else if(pinkyWantsToGoOut) {
-				System.out.println("pinky can go out !");
 				pinkyCanGoOut = true;
 			}
 			else if(clydeWantsToGoOut) {
-				System.out.println("clyde can go out !");
 				clydeCanGoOut = true;
 			}
 			else if(inkyWantsToGoOut) {
-				System.out.println("inky can go out !");
 				inkyCanGoOut = true;
 			}
 		}
@@ -132,7 +121,6 @@ public class GhostsExitBoxThread extends TimerThread {
 	private boolean manageGhostExit(Ghost ghost) {
 		Position doorPosition = maze.getDoorPosition();
 		if(ghost.getCurrentPosition().getX() > doorPosition.getX() - 5 && ghost.getCurrentPosition().getX() < doorPosition.getX() + 5) {
-			System.out.println("ghost go up !");
 			ghost.setState(MovingSpriteState.UP);
 			if(ghost.getCurrentPosition().getY() < doorPosition.getY()-maze.tileDim.height/2){        // if the ghost is out, start its direction thread
 				
@@ -149,11 +137,9 @@ public class GhostsExitBoxThread extends TimerThread {
 			}
 		}
 		else if(ghost.getCurrentPosition().getX() < doorPosition.getX()) {
-			System.out.println("ghost go right !");
 			ghost.setState(MovingSpriteState.RIGHT);
 		}
 		else if(ghost.getCurrentPosition().getX() > doorPosition.getX()) {
-			System.out.println("ghost go left !");
 			ghost.setState(MovingSpriteState.LEFT);
 		}
 		return false;
